f6603e3e9fee48d198d9ac6e292636e910480592
Álvaro Serrano <alsersan20@gmail.com>
2023-05-28 01:07:04 +0200

    Add default kyria layout

---
+++ kyria/v1/rules.mk
@@ -0,0 +1,5 @@
+OLED_ENABLE = yes         # Enable the use of OLED displays
+OLED_DRIVER = SSD1306     
+ENCODER_ENABLE = no       # Disable the use of rotary encoders
+RGB_MATRIX_ENABLE = no    # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = yes     # Enable keyboard RGB underglow
